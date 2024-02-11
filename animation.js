f9c81f66bf97c249c90621d6262c8df6f58e72a2
Vishesh <vaishdobhal122@gmail.com>
2024-02-11 10:51:55 +0530

    Changed some code relate to animation.js

--- animation.js
+++ animation.js
@@ -2,7 +2,7 @@ new TypeIt("#text", {
 
   loop:true
 })
-.type("Manish Jyala", {delay: 1000})
-.delete(12,{delay:500})
+.type("Vishesh Dobhal", {delay: 1000})
+.delete(14,{delay:500})
 .type("Front _ End Dev.",{delay: 1000})
 .go();
