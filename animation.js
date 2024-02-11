d46737ed9b320d7752cfc073b90a5aa8a1aec9a1
Vishesh-dobhal <148621361+Vishesh-dobhal@users.noreply.github.com>
2024-02-11 10:52:49 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -2,7 +2,7 @@ new TypeIt("#text", {
 
   loop:true
 })
-.type("Vishesh Dobhal", {delay: 1000})
+.type("Vishesh", {delay: 1000})
 .delete(14,{delay:500})
 .type("Front _ End Dev.",{delay: 1000})
 .go();
